abdc11d7e1baece30a9169734009c86b12a16f64
North3rnLight3r <kaeganscott26@gmail.com>
2026-08-15 08:46:37 -0500

    add arch-packages.sh to ./manifests

--- manifests/arch-packages.sh
+++ manifests/arch-packages.sh
@@ -1,58 +1,67 @@
 # Minimal graphical substrate and Electron runtime dependencies.
-base-devel
-archiso
-git
-npm
-python
-nodejs-lts-jod
+''''
+"Install run this script to run all packages from ${repo_root}> ./manifests/arch-packages.sh"
+
+''''
+
+sudo pacman -Su \
+base-devel \
+archiso \
+git \
+npm \
+python \
+nodejs-lts-jod 
+
+================================================================================
 # Native Wayland compositor, Plasma visual services, and X11-app compatibility.
-wayland
-xorg-xwayland
-kwin
-plasma-desktop
-plasma-workspace
-systemsettings
-kdialog
-konsole
-breeze
-breeze-gtk
-breeze-icons
-kvantum
-qt6-wayland
-qt6-tools
-qt5-wayland
-xdg-desktop-portal-kde
-kde-gtk-config
-polkit-kde-agent
-dolphin
-gtk3
-mesa
-vulkan-radeon
-amd-ucode
-alsa-lib
-libnotify
-cups
-nss
-libxss
-noto-fonts
-libxcrypt-compat
+--------------------------------------------------------------------------------
+
+sudo pacman -Su \
+wayland \
+xorg-xwayland \
+kwin \
+plasma-desktop \
+plasma-workspace \
+ systemsettings \
+ kdialog \ 
+konsole \
+breeze & breeze-gtk & breeze-icons \
+kvantum \
+qt6-wayland & qt6-tools & qt5-wayland \
+xdg-desktop-portal-kde & kde-gtk-config & polkit-kde-agent \
+dolphin \
+gtk3 \
+mesa \
+vulkan-radeon \
+amd-ucode \
+alsa-lib \
+libnotify \
+cups & nss & libxss \
+noto-fonts & libxcrypt-compat
+
+=========================================================
 # Desktop-session glue. The installer enables greetd after a complete preflight.
-xdg-utils
-xdg-user-dirs
-xdg-desktop-portal
-xdg-desktop-portal-gtk
-gvfs
-chromium
-networkmanager
-pipewire
-pipewire-pulse
-wireplumber
-upower
-power-profiles-daemon
-irqbalance
-bluez
-bluez-utils
-dbus
-dbus-broker
-greetd
-greetd-tuigreet
+----------------------------------------------------------
+sudo pacman -Su \
+xdg-utils &&
+xdg-user-dirs && xdg-desktop-portal && xdg-desktop-portal-gtk \
+gvfs \
+chromium \
+networkmanager \
+pipewire \
+pipewire-pulse \
+wireplumber \
+upower \
+power-profiles-daemon \ 
+irqbalance \
+bluez \
+bluez-utils \
+dbus \
+dbus-broker \
+greetd \
+greetd-tuigreet 
+
+/''''
+"Enjoy This Release of FORGE-OS for linux."
+
+''''
